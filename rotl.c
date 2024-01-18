c04a41e213ccd30561ce6cd7f9a7e24af3ac8153
kamau95 <linuskamau586@gmail.com>
2024-01-18 13:38:32 -0800

    final monty

---
+++ rotl.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+  *f_rotl- spins the stack to the top
+  *@head: stack head pointer
+  *@counter: line_number
+  *Return: null/ no value
+ */
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top element ot the stack
+ * @head: stack head pointer
+ * @counter: line_number
+ * Return: no return/ null
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
